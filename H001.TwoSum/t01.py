334a516a0e6ed8836a176e54b009abce09f03db8
nick <sportingapp+heroku@gmail.com>
2017-02-06 11:41:02 +0800

    hw001 twoSum

---
+++ H001.TwoSum/t01.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+class Solution(object):
+
+    @staticmethod
+    def twoSum(nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        lastidx = len(nums) - 1
+        for idx, val in enumerate(nums):
+            for nextidx in range(lastidx - idx):
+                if val + nums[nextidx + idx + 1] == target:
+                    return [idx, nextidx + idx + 1]
+
+if __name__ == "__main__":
+    nums = [1, 2, 4]
+    target = 3
+    print Solution.twoSum(nums, target)
